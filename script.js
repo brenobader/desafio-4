71dd826eaad71be300ac16c427bb50af1675248d
Breno Bader <81591179+brenobader@users.noreply.github.com>
2022-02-06 22:24:24 -0300

    subindo javascript

---
+++ script.js
@@ -0,0 +1,29 @@
+//1 - crie uma função que exiba uma mensagem no console
+
+//2 - crie uma função que receba o seu nome como (parâmetro) e exiba no console
+
+//3 - crie uma função que receba um nome, um número, e um estilo musical (parâmetros) e exiba no console
+
+//4 - crie uma função que receba um filme, uma música (parâmetros) e exiba no console
+
+function motivacional(){
+    console.log("Lula 2022")
+}
+motivacional ()
+
+
+function nome(meunome){
+    console.log(`meu nome é ${meunome}`)
+}
+nome(`Breno`)
+
+
+function dados1(nome,idade,estilomusical){
+    console.log(`${nome},${idade},${estilomusical}`)
+}
+dados1 (`Breno`, 26,`funk`)
+
+function dados2(filmefavorito,musicafavorita){
+    console.log(`${filmefavorito},${musicafavorita}`)
+}
+dados2(`Meu curta Tichalla`,`Ideologia`)
